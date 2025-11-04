7d875d8eeef214d806d16e2280dbb99dbfecf165
Karen0708-ak <karen.barcia8759@utc.edu.ec>
2025-11-03 23:00:38 -0500

    Se agrego la funcionalidad de visualizar las personas

--- Aplicaciones/Persona/views.py
+++ Aplicaciones/Persona/views.py
@@ -7,7 +7,7 @@ from django.contrib import messages
 
 def inicio(request):
     listadoPersona=Persona.objects.all()
-    return render(request,"inicioper.html",{'empleado':listadoPersona})
+    return render(request,"inicioper.html",{'persona':listadoPersona})
 def nuevapersona(request):
     return render(request,"nuevapersona.html")
 def guardarPersona(request):
@@ -36,7 +36,30 @@ def guardarPersona(request):
     return redirect('inicioper')
 
 
+def eliminarEmpleado(request,id):
+    empleadoEliminar = Empleado.objects.get(id=id)
+    empleadoEliminar.delete()
+    messages.success(request,"Empleado ELIMINADO exitosamente")
+    return redirect('inicioem')
 
+#editar
+def editarEmpleado(request,id):
+    empleadoEditar=Empleado.objects.get(id=id)
+    return render(request,"editarEmpleado.html",{'empleadoEditar':empleadoEditar})
+
+def procesarEdicionEmpleado(request):
+    id=request.POST["id"]
+    nombre = request.POST["nombre"]
+    ubicacion = request.POST["ubicacion"]
+    telefono = request.POST["telefono"]
+    empleado=Empleado.objects.get(id=id)
+    empleado.nombre=nombre
+    empleado.ubicacion=ubicacion
+    empleado.telefono=telefono
+    empleado.save()
+    #mensaje de confirmacion
+    messages.success(request,"Empleado ACTUALIZADO exitosamente")
+    return redirect('inicioem')
 
 
 
