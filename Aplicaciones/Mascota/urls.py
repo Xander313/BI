76dc82fcbddc6fb8f06ce0aad4f1d939a25bd3c3
xander313 <jhonn.roldan8626@utc.edu.ec>
2025-11-04 09:07:45 -0500

    Formualrio de creacion de mascota

--- Aplicaciones/Mascota/urls.py
+++ Aplicaciones/Mascota/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index_mascota"),
+    path('', views.index, name='index_mascota'),
+    path('/newPet', views.newPet, name="newPetUrl"),
+
 ]
--- Aplicaciones/Mascota/views.py
+++ Aplicaciones/Mascota/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+from Aplicaciones.Especie.models import Especie
+from Aplicaciones.Raza.models import Raza
+
 from .models import Mascota
 
 
@@ -9,6 +12,13 @@ def index(request):
         "mascotas": mascotas,
     }
     return render(request, "Mascota/index.html", context)
-def newPet(request):
 
-    return render(request, "Mascota/newPet.html")
+
+def newPet(request):
+    especies = Especie.objects.all().order_by("nombre")
+    razas = Raza.objects.all().order_by("nombre")
+    context = {
+        "especies": especies,
+        "razas": razas,
+    }
+    return render(request, "Mascota/newPet.html", context)
