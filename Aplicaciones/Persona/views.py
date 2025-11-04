14e2623419e8355d6d80d09d0e31bf87e6b9b657
Karen0708-ak <karen.barcia8759@utc.edu.ec>
2025-11-03 22:51:12 -0500

    Se agrego la funcionalidad de guardar Persona

--- Aplicaciones/Persona/views.py
+++ Aplicaciones/Persona/views.py
@@ -8,6 +8,43 @@ from django.contrib import messages
 def inicio(request):
     listadoPersona=Persona.objects.all()
     return render(request,"inicioper.html",{'empleado':listadoPersona})
-def nuevoEmpleado(request):
-    return render(request,"nuevoEmpleado.html")
-#Almacenando los datos de cargo en la Bdd
+def nuevapersona(request):
+    return render(request,"nuevapersona.html")
+def guardarPersona(request):
+    nombres = request.POST["nombres"]
+    apellidos = request.POST["apellidos"]
+    identificacion = request.POST["identificacion"]
+    fecha_nacimiento = request.POST["fecha_nacimiento"]
+    correo = request.POST["correo"]
+    telefono = request.POST["telefono"]
+    direccion = request.POST["direccion"]
+    ciudad = request.POST["ciudad"]
+    ocupacion = request.POST["ocupacion"]
+    nuevapersona=Persona.objects.create(
+            nombres=nombres,
+            apellidos=apellidos,
+            identificacion=identificacion,
+            fecha_nacimiento=fecha_nacimiento,
+            correo=correo,
+            telefono=telefono,
+            direccion=direccion,
+            ciudad=ciudad,
+            ocupacion=ocupacion,
+        )
+    #mensaje de confirmacion
+    messages.success(request,"Persona guardado exitosamente")
+    return redirect('inicioper')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
