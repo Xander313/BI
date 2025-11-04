c728911e436169b8498e02fdbf3a8403d2639fd9
xander313 <jhonn.roldan8626@utc.edu.ec>
2025-11-04 08:57:13 -0500

    lista de mascotas agregadas

--- Aplicaciones/Mascota/views.py
+++ Aplicaciones/Mascota/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Mascota
+
 
 def index(request):
-    return render(request, 'Mascota/index.html')
+    mascotas = Mascota.objects.select_related("especie", "raza").all()
+    context = {
+        "mascotas": mascotas,
+    }
+    return render(request, "Mascota/index.html", context)
+def newPet(request):
+
+    return render(request, "Mascota/newPet.html")
