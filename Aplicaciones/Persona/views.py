131f64b4c99037e2252e0736d6c964436591759c
Karen0708-ak <karen.barcia8759@utc.edu.ec>
2025-11-03 21:39:22 -0500

    Se agrego la logica de la vista de inicio de persona

--- Aplicaciones/Persona/views.py
+++ Aplicaciones/Persona/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+
 
 # Create your views here.
+from .models import Persona
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
+def inicio(request):
+    listadoPersona=Persona.objects.all()
+    return render(request,"inicioem.html",{'empleado':listadoPersona})
